e2101817edacf481a348866a57aadac2f46531a0
Nor Alexanian <dondik123@gmail.com>
2024-01-31 20:20:56 +0100

    docker

--- ContentManager/vite.config.js
+++ ContentManager/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import path from 'path';
 
 export default defineConfig({
     plugins: [
@@ -7,9 +8,29 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
-                'resources/js/article.js'
+                'resources/js/article.js',
             ],
             refresh: true,
         }),
     ],
+    build: {
+        outDir: path.resolve(__dirname, 'public/build'),
+    },
 });
+
+
+// import { defineConfig } from 'vite';
+// import laravel from 'laravel-vite-plugin';
+
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: [
+//                 'resources/css/app.css',
+//                 'resources/js/app.js',
+//                 'resources/js/article.js'
+//             ],
+//             refresh: true,
+//         }),
+//     ],
+// });
