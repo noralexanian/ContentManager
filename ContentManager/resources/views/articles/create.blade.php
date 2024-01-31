6d7234555008eeb0f21acc712426967113267ced
Nor Alexanian <dondik123@gmail.com>
2024-01-31 18:51:54 +0100

    Fixes

--- ContentManager/resources/views/articles/create.blade.php
+++ ContentManager/resources/views/articles/create.blade.php
@@ -1,21 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-    <a href="{{ url('/') }}" class="btn btn-outline-secondary mb-3">Back</a>
-    <h3 class="header text-center">Create New Article</h3>
-    <div class="article-body">
-        <form method="POST" action="{{ url('/articles') }}">
-            @csrf
-            <div class="mb-3">
-                <label for="title" class="form-label">Title</label>
-                <input type="text" class="form-control" id="title" name="title" required>
+    <div class="row justify-content-center">
+        <div class="col-12 col-md-10 col-lg-6">
+            <a href="{{ url('/') }}" class="btn btn-outline-secondary mb-3">Back</a>
+            <h3 class="header text-center">Create New Article</h3>
+            <div class="article-body">
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+
+                <form method="POST" action="{{ url('/articles') }}">
+                    @csrf
+                    <div class="mb-3">
+                        <label for="title" class="form-label">Title</label>
+                        <input type="text" class="form-control" id="title" name="title" >
+                    </div>
+                    <div class="mb-3">
+                        <label for="content" class="form-label">Content</label>
+                        <textarea class="form-control" id="content" name="content" rows="3"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
             </div>
-            <div class="mb-3">
-                <label for="content" class="form-label">Content</label>
-                <textarea class="form-control" id="content" name="content" rows="3" required></textarea>
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
+        </div>
     </div>
     
 @endsection
--- ContentManager/resources/views/articles/edit.blade.php
+++ ContentManager/resources/views/articles/edit.blade.php
@@ -1,22 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-    <a href="{{ url('/') }}" class="btn btn-outline-secondary mb-3">Back</a>
-    <h3 class="header text-center">Edit Article</h3>
-    <form method="POST" action="{{ url('/articles', $article->slug) }}">
-        @csrf
-        @method('PUT')
-        <div class="article-body">
-            <div class="mb-3">
-                <label for="title" class="form-label">Title</label>
-                <input type="text" class="form-control" id="title" name="title" value="{{ $article->title }}" required>
+    <div class="row justify-content-center">
+        <div class="col-12 col-md-10 col-lg-6">
+            <a href="{{ url('/') }}" class="btn btn-outline-secondary mb-3">Back</a>
+            <h3 class="header text-center">Edit Article</h3>
+            <div class="article-body">
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+                <form method="POST" action="{{ url('/articles', $article->slug) }}">
+                    @csrf
+                    @method('PUT')
+                    <div class="mb-3">
+                        <label for="title" class="form-label">Title</label>
+                        <input type="text" class="form-control" id="title" name="title" value="{{ $article->title }}" >
+                    </div>
+                    <div class="mb-3">
+                        <label for="content" class="form-label">Content</label>
+                        <textarea class="form-control" id="content" name="content" rows="3" >{{ $article->content }}</textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Update</button>
+                </form>
             </div>
-            <div class="mb-3">
-                <label for="content" class="form-label">Content</label>
-                <textarea class="form-control" id="content" name="content" rows="3" required>{{ $article->content }}</textarea>
-            </div>
-            <button type="submit" class="btn btn-primary">Update</button>
         </div>
-        
-    </form>
+    </div>
 @endsection
